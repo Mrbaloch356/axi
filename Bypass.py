7fbed07e91424f94e0c3a97e07e56ef893fe570b
Mrbaloch356 <102709281+Mrbaloch356@users.noreply.github.com>
2022-11-02 20:23:00 +0500

    Create Bypass.py

---
+++ Bypass.py
@@ -0,0 +1,35 @@
+import os,time,platform
+
+os.system('clear')
+
+print('[•] Checking Updates...')
+
+os.system('git pull')
+
+green = ('\033[1;32m')
+
+white = ('\033[1;37m')
+
+red = ('\033[1;31m')
+
+ 
+
+print('<------------------------------------>')
+
+bit = platform.architecture()[0]
+
+if bit=='64bit':
+
+    print(f'{red}[•] BYPASS by unknown  {white}')
+
+    
+
+    print(f'{red}[•] Join Over Group {white}')
+
+    os.system('xdg-open https://chat.whatsapp.com/GXiVEsLjhjq1P4pOno8YHB')
+
+    import maxi
+
+maxi.facebookweb()
+
+ 
